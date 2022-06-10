c65bf70f770936e50169ca1f4ef16124f40b34e8
Alexey Abramenko <a.abramenko@chita.ru>
2022-06-10 17:09:37 +0900

    Updates

--- src/locomotive-scroll.d.ts
+++
@@ -1,1 +0,0 @@
-export const locomotiveScroll: any = require("locomotive-scroll");
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-// import router from './router'
-// import store from './store'
-
-const app = createApp(App);
-app.mount("#app");
-
-//createApp(App).use(store).use(router).mount('#app')
